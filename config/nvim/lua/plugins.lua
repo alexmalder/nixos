87d85d3b91a93d271028c989783bf05d6f9a36ec
alexmalder <alexmalder>
2026-09-18 20:42:54 +0300

    switch nvim theme to monochrome

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -90,22 +90,17 @@ require("lazy").setup({
     'makerj/vim-pdf'
   },
   {
-    "aktersnurra/no-clown-fiesta.nvim",
-	  theme = "dark", -- supported themes are: dark, dim, light
-	  transparent = false, -- Enable this to disable the bg color
-	  styles = {
-	    -- You can set any of the style values specified for `:h nvim_set_hl`
-	    comments = {},
-	    functions = {},
-	    keywords = {},
-	    lsp = {},
-	    match_paren = {},
-	    type = {},
-	    variables = {},
-	  },
+    "kdheepak/monochrome.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
     config = function()
-        vim.cmd("colorscheme no-clown-fiesta")
-    end,
+      vim.cmd.colorscheme('monochrome')
+    end
   },
   {
     "hedyhli/outline.nvim",
