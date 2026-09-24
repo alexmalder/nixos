9274567a534dd0088bccc5894e0cb76fcd4755a2
alexmalder <alexmalder>
2026-09-24 16:06:49 +0300

    ansible role added (ubuntu desktop)

--- push-and-apply.sh
+++
@@ -1,9 +0,0 @@
-# simple copy and apply script
-
-cp config/home-manager/home.nix "$HOME/.config/home-manager/home.nix"
-
-sudo cp etc/nixos/configuration.nix /etc/nixos/configuration.nix
-
-#sudo nixos-rebuild switch
-
-nix-shell -p home-manager --run "home-manager switch"
